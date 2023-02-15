ee014f5dee953d09646e3736c3439b11c4eada8b
namsy8471 <namsy8471@naver.com>
2023-02-15 15:46:16 +0900

    연습용 유니티 파일 업로드
    연습용 유니티 파일 업로드

---
+++ Assets/Script/LoopManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TwoFor();
+        UserSum();
+        UserSum(15);
+        UserSum(10, 15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void TwoFor()
+    {
+        for(int i = 2; i <= 9; i++)
+        {
+            for(int k = 1; k <= 9; k++)
+            {
+                Debug.Log(i + " * " + k + " = " + (i * k));
+            }
+        }
+    }
+
+    void UserSum()
+    {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) 
+            sum += i;
+        Debug.Log(sum);
+    }
+
+    void UserSum(int num)
+    {
+        int sum = 0;
+        for (int i = 1; i <= num; i++)
+            sum += i;
+        Debug.Log(sum);
+    }
+
+    void UserSum(int num1, int num2)
+    {
+        int sum = 0;
+        for (int i = num1; i <= num2; i++)
+            sum += i;
+        Debug.Log(sum);
+    }
+
+}
---
+++ Assets/Script/ifManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifManager : MonoBehaviour
+{
+    public GameObject[] Mons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Monster()
+    {
+        int monNum = Random.Range(0, Mons.Length);
+
+        if (GameObject.FindGameObjectWithTag("Enemy"))
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+
+        switch (monNum)
+        {
+            case 0:
+                {
+                    Debug.Log("몬스터 0번");
+                    Instantiate(Mons[0]);
+                }break;
+            case 1: 
+                {
+                    Debug.Log("몬스터 1번");
+                    Instantiate(Mons[1]);
+                }
+                break;
+            case 2:
+                {
+                    Debug.Log("몬스터 2번");
+                    Instantiate(Mons[2]);
+                }
+                break;
+            case 3:
+                {
+                    Debug.Log("몬스터 3번");
+                    Instantiate(Mons[3]);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
